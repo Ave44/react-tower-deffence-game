3d70def1d627265e005130cf6d731901e8e56677
Paweł Olszewski <monte.paw@gmail.com>
2021-10-18 19:03:42 +0200

    Rozwiązanie zadania 2

--- lab02/zad2.py
+++ lab02/zad2.py
@@ -1,7 +1,19 @@
-import paramiko
-print('test')
+import paramiko as paramiko
+import os
+from dotenv import load_dotenv
 
-client = SSHClient()
-client.load_system_host_keys()
-client.connect('ssh sigma')
-stdin, stdout, stderr = client.exec_command('cat test.txt')
+load_dotenv()
+
+host = os.getenv('host')
+port = os.getenv('port')
+connection_username = os.getenv('connection_username')
+connection_password = os.getenv('connection_password')
+command = os.getenv('command')
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(host, port, connection_username, connection_password)
+
+stdin, stdout, stderr = client.exec_command(command)
+result = stdout.readlines()
+print(result)
