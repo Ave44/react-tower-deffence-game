bb41ec08cd9d94b6f31f479e37060cc56f3d7a5e
Paweł Olszewski <monte.paw@gmail.com>
2021-11-10 22:47:53 +0100

    Zapis postępów - funkcjonalność gracza

--- lab05/backend/games.js
+++ lab05/backend/games.js
@@ -19,14 +19,14 @@ router.get('/:key', async (req, res) => {
 
 router.post('/', async (req, res) => {
     const key = req.body.key
-    client.setex(key, 300, "000000000")
+    client.setex(key, 300, "01 02 03 04 05 06 07 08 09")
     return res.json(req.body);
 });
 
 router.post('/:key', async (req, res) => {
     const key = req.params.key
-    const data = req.body.data
-    client.setex(key, 180, data)
+    const game = req.body.game
+    client.setex(key, 300, game)
     return res.json(req.body);
 });
 
--- lab05/frontend/src/Game.js
+++ lab05/frontend/src/Game.js
@@ -1,48 +1,58 @@
 import axios from 'axios';
 import { useParams } from "react-router-dom";
 import { useState ,useEffect } from "react";
-import { v4 as uuidv4 } from 'uuid';
 
 function Game() {
 
     const gameId = useParams().gameId
-    const [board, setBoard] = useState('000000000')
+    const [board, setBoard] = useState('01 02 03 04 05 06 07 08 09')
 
     useEffect(() => {
         axios.get(`http://localhost:5000/games/${gameId}`)
             .then(response => {
-              console.log(response.data)
+              console.log("initial board:", response.data)
               setBoard(response.data.game)
             })
             .catch(err => console.log(err))
     }, [gameId])
 
+    function newBoard(index, symbol) {
+        if(index !== '9') {
+            return board.slice(0, index*3-3)+symbol+index+" "+board.slice(index*3)
+        }
+        return board.slice(0, index*3-3)+symbol+index
+    }
+
+    function handleClick(index) {
+        const game = newBoard(index, 'x')
+        axios.post(`http://localhost:5000/games/${gameId}`, {game})
+        .then(response => {
+          console.log(response.data)
+          setBoard(response.data.game)
+        })
+        .catch(err => console.log(err))
+    }
+
     function draw(board) {
-        if (board !== null) {
-            return board.split("").map(i => {return match(i)})
+        console.log(board, "draw")
+        if (board !== null && board !== undefined) {
+            return board.split(" ").map(i => {return match(i)})
         }
         return "Your game have expired or had never existed!"
     }
 
     function match(item) {   
-        if( item === 'x' ) {
-            return field('x')
+        if( item[0] === 'x' ) {
+            return <div className="field" key={item[1]}>X</div>
         }
-        if( item === 'o' ) {
-            return field('o')
+        if( item[0] === 'o' ) {
+            return <div className="field" key={item[1]}>O</div>
         }
-        return field('')
-    }
-
-    function field(type) {
-        if(type === 'x') {
-            return <div className="field" key={uuidv4()}>X</div>
-        }
-        if(type === 'o') {
-            return <div className="field" key={uuidv4()}>O</div>
-        }
-        return <div className="corners"><button className="field" key={uuidv4()}>_</button></div>
-    }
+        return (
+            <div className="corners" key={item[1]}>
+                <button className="small_button" onClick={() => {handleClick(item[1])}}></button>
+            </div>
+        )}
 
     return (
         <div className="board">
