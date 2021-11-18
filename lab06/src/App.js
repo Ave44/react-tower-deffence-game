3619f8b429528884929d84b5d737c4b6c65f8f46
Paweł Olszewski <monte.paw@gmail.com>
2021-11-19 00:11:57 +0100

    Pełne rozwiązanie zadania 2

--- lab06/src/App.js
+++ lab06/src/App.js
@@ -5,37 +5,49 @@ import { useState } from 'react';
 function App() {
 
   const [isLogged, setIsLogged] = useState(false)
+  const [user, setUser] = useState("")
+  const [liczba, setLiczba] = useState(0)
+
+  // const inMinute = new Date(new Date().getTime() + 1 * 60 * 1000); // sposób na ustawienie czasu życia ciastka na 1 minutę
 
   function handleClick() {
-    const user = document.getElementById('name').value + document.getElementById('surname').value
+    const user = document.getElementById('name').value + ' ' + document.getElementById('surname').value
     const liczbaOdwiedzin = Cookies.get(user)
     if (liczbaOdwiedzin !== undefined) {
       Cookies.set(user, parseInt(liczbaOdwiedzin) + 1, { expires: 7 })
     }
-    else (
+    else {
       Cookies.set(user, '1', { expires: 7 })
-    )
+    }
     setIsLogged(true)
-    console.log(liczbaOdwiedzin)
-    console.log(user)
+    setUser(document.getElementById('name').value + " " + document.getElementById('surname').value)
+    setLiczba(Cookies.get(user))
   }
 
-  function greet(isLogged) {
-    if (isLogged === false) {
-      return <div></div>
+  function handleRemove() {
+    const cookies = Cookies.get()
+    for(const cookie in cookies) {
+      Cookies.remove(cookie)
     }
-    const user = document.getElementById('name').value + document.getElementById('surname').value
-    return <div>Witaj {user} , odwiedziłeś naszą stronę {Cookies.get(user)} razy.</div>
+    setIsLogged(false)
+    document.getElementById('name').value = ""
+    document.getElementById('surname').value = ""
+  }
+
+  function greet(isLogged, user, liczba) {
+    if (isLogged === false) { return <div></div> }
+    return <h3>Witaj {user} , odwiedziłeś naszą stronę {liczba} razy.</h3>
   }
 
   return (
     <div>
-      <div>{greet(isLogged)}</div>
+      <div>{greet(isLogged, user, liczba)}</div>
       <h2>Imie</h2>
       <input type='text' id='name'/>
       <h2>Nazwisko</h2>
       <input type='text' id='surname'/>
-      <button onClick={()=>{handleClick(0)}}>Potwierdź</button>
+      <button onClick={()=>{handleClick()}}>Potwierdź</button>
+      <button onClick={()=>{handleRemove()}}>Usun wszystkie ciasteczka</button>
     </div>
   );
 }
