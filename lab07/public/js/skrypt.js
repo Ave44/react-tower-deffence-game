46e000e68ee3e594bde36b21bbdd39f7d7f467c9
Paweł Olszewski <monte.paw@gmail.com>
2021-12-01 15:15:08 +0100

    Usprawnienie działania

--- lab07/public/js/skrypt.js
+++ lab07/public/js/skrypt.js
@@ -1,21 +1,9 @@
-// const createRoom = () => {
-//     const roomName = document.getElementById('create').value
-//     console.log(roomName)
+const createRoom = (login) => {
+    console.log(login)
 
-//     window.addEventListener("load", function (event) {
-//         socket = io.connect('http://' + location.host);
-
-//         socket.on('connect', function () {
-//             console.log('Nawiązano połączenie przez Socket.IO' + roomName);
-//             socket.emit("test", "czesc");
-//         });
-//     });
-// }
-
-window.addEventListener("load", function (event) {
     socket = io.connect('http://' + location.host);
 
-    socket.on('connect', function (login) {
+    socket.on('connect', function () {
         console.log('Nawiązano połączenie przez Socket.IO');
         socket.emit("test", `Urzytkownik ${login} dołączył do czatu`);
 
@@ -29,4 +17,24 @@ window.addEventListener("load", function (event) {
             showMessage('Urzytkownik opuścił czat')
         });
     });
-});
+
+}
+
+// window.addEventListener("load", function (event) {
+//     socket = io.connect('http://' + location.host);
+
+//     socket.on('connect', function (login) {
+//         console.log('Nawiązano połączenie przez Socket.IO');
+//         socket.emit("test", `Urzytkownik ${login} dołączył do czatu`);
+
+//         socket.on('test', (msg) => {
+//             console.log(msg)
+//             showMessage(msg)
+//         })
+
+//         socket.on('disconnect', () => {
+//             console.log('Urzytkownik opuścił czat')
+//             showMessage('Urzytkownik opuścił czat')
+//         });
+//     });
+// });
